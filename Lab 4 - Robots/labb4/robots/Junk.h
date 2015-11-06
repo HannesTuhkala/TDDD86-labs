a69deb5bd07059ff047f4267823b066727f895de
malcx95 <trekommafem2@gmail.com>
2015-11-06 08:17:38 +0100

    Lade till labb 4

---
+++ Lab 4 - Robots/labb4/robots/Junk.h
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) David Wolfe, 1999.  All rights reserved.
+ * Ported to Qt and adapted for TDDD86, 2015.
+ */
+
+#ifndef JUNK_H
+#define JUNK_H
+
+#include "Unit.h"
+#include "Robot.h"
+#include <QGraphicsScene>
+
+class Junk : public Robot {
+public:
+    Junk();
+    Junk(Unit c);
+	bool isJunk() const override;
+
+    /*
+    * Draws this junk onto the given QGraphicsScene.
+    */
+    void draw(QGraphicsScene* scene) const override;
+};
+
+#endif // JUNK_H
