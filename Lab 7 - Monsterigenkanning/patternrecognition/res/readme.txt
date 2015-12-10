7eec1f485249a5ab1a2d13f325616a40c3e0971c
Malcolm <trekommafem2@gmail.com>
2015-12-10 17:42:54 +0100

    Döpte om labb 7 eftersom YCM fackade med åäö

---
+++ Lab 7 - Monsterigenkanning/patternrecognition/res/readme.txt
@@ -0,0 +1,56 @@
+/**********************************************************************
+ *  Mönsterigenkänning readme.txt
+ **********************************************************************/
+
+/**********************************************************************
+ *  Empirisk    Fyll i tabellen nedan med riktiga körtider i sekunder
+ *  analys      när det känns vettigt att vänta på hela beräkningen.
+ *              Ge uppskattningar av körtiden i övriga fall.
+ *
+ **********************************************************************/
+    
+      N		brute [s]      sortering [s]
+ ----------------------------------
+    150		0.008			0.001
+    200		0.019			0.003
+    300		0.070			0.006
+    400		0.155			0.018
+    800		1.087			0.065
+   1600		8.576			0.208
+   3200		68.321			0.704
+   6400		548.040			3.119
+  12800		4395.437		14.028
+
+
+/**********************************************************************
+ *  Teoretisk   Ge ordo-uttryck för värstafallstiden för programmen som
+ *  analys      en funktion av N. Ge en kort motivering.
+ *	
+ **********************************************************************
+ *	Brute force.
+ *	Det värsta fallet som kan inträffa för denna algoritm är att alla 
+ *	punkter i filen ligger på en och samma linje. Detta är för att 
+ *	programmet försöker optimera bort några beräkningar genom att inte 
+ *	loopa genom alla punkter för den fjärde punkten om de tre första 
+ *	första punkterna inte ligger på samma linje. Om alla punkter däremot
+ *	ligger på samma linje, kommer den alltid att behöva kolla den fjärde
+ *	punkten. Detta gör att programmet måste besöka alla kombinationer av 
+ *	fyra punkter i filen, alltså nCr(n, 4) besök. Alla andra steg i 
+ *	programmet, som sortering och inläsning, tar konstant, linjär 
+ *	nlog(n) tid, så dessa kan försummas eftersom nCr(n, 4) växer mycket
+ *	snabbare än allt annat.
+ *	
+ *	Algoritmen ligger därför i O(nCr(n, 4)) i det värsta fallet.
+ *	
+ **********************************************************************
+ *	
+ *	
+ *	
+ *	
+ *	
+ *	
+ **********************************************************************/
+
+Brute:
+
+Sortering:
---
+++ Lab 7 - Monsterigenkanning/patternrecognition/src/fast.cpp
@@ -0,0 +1,163 @@
+#include <QApplication>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <chrono>
+#include "Point.h"
+
+using namespace std;
+// constants
+static const int SCENE_WIDTH = 512;
+static const int SCENE_HEIGHT = 512;
+
+/*
+ * A data structure that helps us to compare a point to a starting point.
+ */
+struct PointComparison {
+	/*
+	 * The point which all other points compare to.
+	 */
+	Point* comparePoint;
+	/*
+	 * The point to compare with.
+	 */
+	Point* thisPoint;
+	/*
+	 * The slope between the points.
+	 */
+	double slope;
+	/*
+	 * Constructs a PointComparison and calculates the slope between the two points.
+	 */
+	PointComparison(Point* comparePoint, Point* thisPoint) {
+		this->comparePoint = comparePoint;
+		this->thisPoint = thisPoint;
+		this->slope = comparePoint->slopeTo(*thisPoint);
+	}
+
+	bool operator <(const PointComparison &other) const {
+		if (this->slope != other.slope) {
+			return this->slope < other.slope;
+		} else {
+			return *this->thisPoint < *other.thisPoint;
+		}
+	}
+	
+	bool operator >(const PointComparison &other) const {
+		if (this->slope != other.slope) {
+			return this->slope > other.slope;
+		} else {
+			return *this->thisPoint > *other.thisPoint;
+		}
+	}
+};
+
+void render_points(QGraphicsScene* scene, const vector<Point>& points) {
+    for(const auto& point : points) {
+        point.draw(scene);
+    }
+}
+
+void render_line(QGraphicsScene* scene, const Point& p1, const Point& p2) {
+    p1.lineTo(scene, p2);
+}
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    
+	// open file
+    string filename = "input12800.txt";
+    ifstream input;
+    input.open(filename);
+
+
+    // read points from file
+    int N;
+    int x;
+    int y;
+
+    input >> N;
+
+    // the vector of points
+    vector<Point> points;
+	// a vector of pointers to the points in points
+	vector<Point*> pointPointers;
+
+    for (int i = 0; i < N; ++i) {
+        input >> x >> y;
+		Point* p = new Point(x, y);
+        points.push_back(*p);
+		pointPointers.push_back(p);
+    }
+    input.close();
+	
+    // setup graphical window
+    QGraphicsView *view = new QGraphicsView();
+    QGraphicsScene *scene = new QGraphicsScene(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
+    view->setScene(scene);
+    // draw points to screen all at once
+    render_points(scene, points);
+    view->scale(1, -1); //screen y-axis is inverted
+    view->resize(view->sizeHint());
+    view->setWindowTitle("Fast Pattern Recognition");
+    view->show();
+    auto begin = chrono::high_resolution_clock::now();
+	//do all of this using each point as starting point
+	for (int startingPoint = 0; startingPoint < N; ++startingPoint) {
+		vector<PointComparison> comparisons;
+		//iterate over all except the starting point. Instead of
+		//checking each point if it is a starting point, iterate first
+		//over all points until the starting point, and then after.
+		for (int i = 0; i < startingPoint; ++i) {
+			//for every point that isn't the starting point, create a PointComparison of 
+			//the starting point pointer and the point pointer at index i.
+			comparisons.push_back(PointComparison(pointPointers[startingPoint], pointPointers[i]));
+		}
+		for (int i = startingPoint + 1; i < N; ++i) {
+			//do the same here.
+			comparisons.push_back(PointComparison(pointPointers[startingPoint], pointPointers[i]));
+		}
+		//sort the comparisons by slope
+		sort(comparisons.begin(), comparisons.end());
+
+		//start finding collinear lines. Create an index variable.
+		int i = 0;
+		//since we are checking two points ahead, and checking every point except the starting point
+		//we check from i = 0 to N-1-2 = N-3.
+		while (i < N - 3) {
+			//get the slopes of this index and the one two indexes away
+			double slope1 = comparisons[i].slope;
+			double slope2 = comparisons[i + 2].slope;
+			//if they are equal, that means we have found four aligned points (including the startingPoint)!
+			//Lets see if we can find more...
+			if (slope1 == slope2) {
+				int j = i + 3;
+				while (slope1 == comparisons[j].slope && j < N - 1) {
+					//we found another aligned point!
+					j++;
+				}
+				//okay, so the compared point at comparisons[j] was not aligned, so we will
+				//just draw a line from the starting point to the one at j-1 (since that was the 
+				//last aligned point). We use comparisons[0] to get the starting point but
+				//we could just use any element in comparisons since they all have the same
+				//pointer to the starting point.
+                render_line(scene, *comparisons[0].comparePoint, *comparisons[j-1].thisPoint);
+                a.processEvents(); // show rendered line
+				//continue searching from where the last aligned point was found.
+				i = j - 1;
+			} else {
+				//okay, we didn't find aligned points here, increment and continue.
+				i++;
+			}
+		}
+	}
+	//And we're done! Pause the timer.
+    auto end = chrono::high_resolution_clock::now();
+    cout << "Computing line segments took "
+         << std::chrono::duration_cast<chrono::milliseconds>(end - begin).count()
+         << " milliseconds." << endl;
+	return a.exec();
+}
